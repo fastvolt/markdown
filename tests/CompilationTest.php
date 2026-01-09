6990f5eb6c000714c88c64e3dd51d55ae2029f20
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2026-01-09 19:00:17 +0100

    No need to manually call the autoloader in unit tests

--- tests/CompilationTest.php
+++ tests/CompilationTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
-
 use FastVolt\Helper\Markdown;
 use FastVolt\Helper\Markdown\Enums\MarkdownEnum;
 use PHPUnit\Framework\TestCase;
